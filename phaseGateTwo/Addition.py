4fc0cbe232310f5775bb0bd11f9620edc7b6100e
Bolarji849 <mbolaji209@gmail.com>
2024-10-04 15:32:47 +0100

    Addition.py

---
+++ phaseGateTwo/Addition.py
@@ -0,0 +1,16 @@
+def addition(number1, number2):
+
+	
+	first_digit = int(number1)
+	
+	second_digit = int(number2)
+	 
+	addition  = first_digit + second_digit
+	
+	sum_of_result = str (addition)
+
+	print( sum_of_result )
+
+
+
+addition("3","4")
