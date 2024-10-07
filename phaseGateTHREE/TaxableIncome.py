80587f7bd722bd7b286b289d6f35a1003bdcd698
Bolarji849 <mbolaji209@gmail.com>
2024-10-07 17:13:43 +0100

    TaxableIncome.py

---
+++ phaseGateTHREE/TaxableIncome.py
@@ -0,0 +1,24 @@
+
+print('''
+	single Tax - 1
+	………………………………………………..
+	
+	married_joint_Tax -2 
+	……………………………………………………….
+	married_seprately - 3
+	………………………………………………………..
+	head_household_tax - 4
+	……………………………………………………………..
+				''')
+
+
+userInput =  int(input("Enter menu number "))
+Taxable-amount =  int(input("Enter menu number "))
+balance = 0
+if( userInput ==1)
+	balance = Taxable-amount  - 8350
+		if(balance > 0 and <= 8350):
+		 	balance * 0.10
+
+		if(balance >8351 and <= 33950):
+			balance * 15
